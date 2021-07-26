ba62eea65bb1a491f5804497f6ae9aed3b0511d5
Brian Welsh <bwelsh@jhacorp.com>
2021-07-26 11:05:03 -0500

    cleanup old fxcop files

--- tools/CommonAssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-//-----------------------------------------------------------------------
-// <copyright file="CommonAssemblyInfo.cs" company="Brian Welsh, welshnson.com">
-//     Copyright (c) Brian Welsh, welshnson.com. All rights reserved.
-// </copyright>
-//-----------------------------------------------------------------------
-
-using System;
-
-[assembly: CLSCompliant(false)]
